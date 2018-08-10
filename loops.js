262a5c9ea3f7d9ce2c46134e717fbdc46493943f
Eric Klimowich <eklimowich@hotmail.com>
2018-08-10 03:08:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -13,5 +13,11 @@ function whileLoop(num) {
 }
 
 function doWhileLoop(array) {
-  
+  function incrementVariable() {
+  i = i + 1;
+}
+ 
+do {
+  array.pop();
+} while ((array.length > 0) || (i < 5));
 }
