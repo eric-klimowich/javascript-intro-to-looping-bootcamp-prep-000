68e1048c55b18db145dd1692c36cc85a74bf2612
Eric Klimowich <eklimowich@hotmail.com>
2018-08-10 02:45:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,13 +1,6 @@
 function forLoop(array) {
   for (i = 0; i < 25; i++) {
-    array.push
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
   }
 }
   
-  
-  
-  for (i = 0; i < 25; i++) {
-  array.push(`I am ${i} strange loop.`);
-}
-
-return array;
